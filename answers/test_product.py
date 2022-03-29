d1e82bd135d0175e39edf038c37fc3308d476719
Godwin Aden <godwinaden@gmail.com>
2022-03-29 10:42:10 +0100

    =edited answers

--- answers/test_product.py
+++ answers/test_product.py
@@ -6,29 +6,30 @@ from async_utility import asyncio_run, asyncio_gather
 from product import Product
 
 tracemalloc.start()
+api_result_data = [
+    [
+        {"product": "Shoes", "price": 35, "rating": 4.2},
+        {"product": "White Hat", "price": 21, "rating": 4.8},
+        {"product": "Blue Gown", "price": 28, "rating": 3.5},
+        {"product": "Black Jewelry", "price": 99, "rating": 2.2},
+        {"product": "Wedding Gown", "price": 56, "rating": 4.6},
+        {"product": "Ankara", "price": 126, "rating": 4.0},
+        {"product": "Sandwich", "price": 68, "rating": 3.7},
+    ],
+    [
+        {"product": "Bags", "price": 350, "rating": 2.2},
+        {"product": "Mattress", "price": 210, "rating": 1.8},
+        {"product": "Blue Shoe", "price": 380, "rating": 4.5},
+        {"product": "Green Pen", "price": 94, "rating": 5.0},
+        {"product": "Brown Gown", "price": 506, "rating": 3.6},
+        {"product": "Fish", "price": 786, "rating": 4.8},
+        {"product": "Bread", "price": 968, "rating": 4.7},
+    ]
+]
 
 
 class TestProduct:
     product = Product()
-
-    @pytest.fixture
-    def mocked_api_result(self):
-        return [
-            {"product": "Shoes", "price": 35, "rating": 4.2},
-            {"product": "White Hat", "price": 21, "rating": 4.8},
-            {"product": "Blue Gown", "price": 28, "rating": 3.5},
-            {"product": "Black Jewelry", "price": 99, "rating": 2.2},
-            {"product": "Wedding Gown", "price": 56, "rating": 4.6},
-            {"product": "Ankara", "price": 126, "rating": 4.0},
-            {"product": "Sandwich", "price": 68, "rating": 3.7},
-        ]
-
-    @pytest.fixture
-    def test_methods_to_test(self):
-        return [
-            self.product.get_products,
-            self.product.get_highly_rated
-        ]
 
     def test_is_key_valid(self):
         test_keys = ['rating', 'product', 'price']
@@ -71,18 +72,7 @@ class TestProduct:
                 else:
                     pytest.fail('Unknown Key used here')
 
-    @pytest.mark.parametrize('api_results', [
-        mocked_api_result,
-        [
-            {"product": "Bags", "price": 350, "rating": 2.2},
-            {"product": "Mattress", "price": 210, "rating": 1.8},
-            {"product": "Blue Shoe", "price": 380, "rating": 4.5},
-            {"product": "Green Pen", "price": 94, "rating": 5.0},
-            {"product": "Brown Gown", "price": 506, "rating": 3.6},
-            {"product": "Fish", "price": 786, "rating": 4.8},
-            {"product": "Bread", "price": 968, "rating": 4.7},
-        ]
-    ])
+    @pytest.mark.parametrize('api_results', api_result_data)
     def test_get_highly_rated(self, api_results):
         self.product.all_products = api_results
         highly_rated_products = self.product.get_highly_rated()
@@ -91,7 +81,11 @@ class TestProduct:
 
     @pytest.mark.total_time(td(milliseconds=100))
     @pytest.mark.total_memory("2 MB")
-    @pytest.mark.parametrize('methods', test_methods_to_test)
-    def test_method_performance(self, benchmark, methods):
-        benchmark(methods)
+    @pytest.mark.asyncio
+    async def test_get_product_performance(self, benchmark):
+        benchmark(self.product.get_products)
 
+    @pytest.mark.total_time(td(milliseconds=100))
+    @pytest.mark.total_memory("2 MB")
+    def test_get_highly_rated_performance(self, benchmark):
+        benchmark(self.product.get_highly_rated)
